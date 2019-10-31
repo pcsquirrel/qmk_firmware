b58cb6c73e8009fb1c15a45f496b6b045cc14727
Philipp Eichhorn <phc.eichhorn@mci4me.at>
2019-10-31 19:31:53 +0100

    Update onekey.c

--- keyboards/handwired/onekey/onekey.c
+++ keyboards/handwired/onekey/onekey.c
@@ -1,20 +1,2 @@
 #include "onekey.h"
 
-void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable=true;
-  debug_matrix=true;
-  debug_keyboard=true;
-  //debug_mouse=true;
-
-  print("hello\n");
-   dprint("debug\n");
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  // If console is enabled, it will print the matrix position and status of each key pressed
-#ifdef CONSOLE_ENABLE
-    uprintf("KL: kc: %u, col: %u, row: %u, pressed: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed);
-#endif
-  return true;
-}
